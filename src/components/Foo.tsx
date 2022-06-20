b472fd0b35862ff04eddfa0697511ef5913d139d
okym <cndd97@gmail.com>
2022-06-21 00:49:02 +0900

    first commit

---
+++ src/components/Foo.tsx
@@ -0,0 +1,6 @@
+import React from 'react'
+import { Button, ButtonGroup } from '@chakra-ui/react'
+
+export default function Foo() {
+  return <Button colorScheme='blue'>Button</Button>
+}
---
+++ src/pages/index.tsx
@@ -0,0 +1,6 @@
+import React from 'react'
+import Foo from '../components/Foo'
+
+export default function HomePage() {
+  return <Foo />
+}
